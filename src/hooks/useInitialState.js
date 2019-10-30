9002e8d6002b26b360a10b95458548275f178ab2
elcordova <erick.lenin.03@gmail.com>
2019-10-29 22:50:50 -0500

    add hooks

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+
+const useInitalState = (API) => {
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+  return videos;
+};
+
+export default useInitalState;
