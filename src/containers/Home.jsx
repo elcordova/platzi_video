eeaf5bec32fc979b4df756d7e9f9d836ea4d26fb
elcordova <erick.lenin.03@gmail.com>
2019-11-08 21:25:26 -0500

    react router implemented

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import Header from '../components/Header';
 import '../assets/styles/App.scss';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitalState from '../hooks/useInitialState';
 
 const API = 'http://localhost:3000/initialState';
-const App = () => {
+const Home = () => {
   const initialState = useInitalState(API);
   console.log(initialState);
   return (
-    <div className='App'>
-      <Header />
+    <>
       <Search />
       {
         initialState.mylist.length > 0 && (
@@ -40,10 +37,8 @@ const App = () => {
           </Categories>
         )
       }
-
-      <Footer />
-    </div>
+    </>
   );
 };
 
-export default App;
+export default Home;
---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+  <>
+    <h1>Not Found F**k</h1>
+  </>
+);
+
+export default NotFound;
